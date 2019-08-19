9a0dc644652e2e65c5f26d924e63d79f8642cdda
Alfonso Fernando Álvarez <ponchoalv@gmail.com>
2019-08-19 18:03:10 -0300

    added new versión of the builded app

--- resources/migrations/20190211180058-users.up.sql
+++ resources/migrations/20190211180058-users.up.sql
@@ -1,4 +1,7 @@
 CREATE TABLE "users" (
 	"username" VARCHAR(50) NULL DEFAULT NULL,
 	"password" VARCHAR(50) NULL DEFAULT NULL
-)
+);
+INSERT INTO users
+(username, "password")
+VALUES('aalvarez', 'blueturtle15');

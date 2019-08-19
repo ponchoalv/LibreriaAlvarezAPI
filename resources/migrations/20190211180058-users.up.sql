61ffb6c12d3b25f647c4335ccbfa71d07830dd65
Alfonso Fernando Álvarez <ponchoalv@gmail.com>
2019-08-19 18:59:08 -0300

    correción script de creación que falló

--- resources/migrations/20190211180058-users.up.sql
+++ resources/migrations/20190211180058-users.up.sql
@@ -1,7 +1,4 @@
 CREATE TABLE "users" (
 	"username" VARCHAR(50) NULL DEFAULT NULL,
 	"password" VARCHAR(50) NULL DEFAULT NULL
-);
-INSERT INTO users
-(username, "password")
-VALUES('aalvarez', 'blueturtle15');
+)
